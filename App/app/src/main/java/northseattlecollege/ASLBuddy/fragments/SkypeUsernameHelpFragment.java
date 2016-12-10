23a3f21ff372fcdd5c216528a3b7d3c9e4f9552e
changsai39 <changsai39@gmail.com>
2016-12-09 16:29:16 -0800

    Initial commit with fragment still in progress

---
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/fragments/SkypeUsernameHelpFragment.java
@@ -0,0 +1,23 @@
+package northseattlecollege.ASLBuddy.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import northseattlecollege.ASLBuddy.R;
+
+/**
+ * Created by Sai Chang on 12/9/2016.
+ */
+public class SkypeUsernameHelpFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /**
+         * Inflate the layout for this fragment
+         */
+        return inflater.inflate(R.layout.fragment_skype_profile_help, container, false);
+
+
+    }
+}
