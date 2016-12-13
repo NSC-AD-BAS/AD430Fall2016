ddb553808e2af4b2ef9f677eaf27bac53fcbd3f0
changsai39 <changsai39@gmail.com>
2016-12-12 16:29:18 -0800

    Skype username help fragment complete and ready to be added to the desired activity

--- App/app/src/main/java/northseattlecollege/ASLBuddy/fragments/SkypeUsernameHelpFragment.java
+++ App/app/src/main/java/northseattlecollege/ASLBuddy/fragments/SkypeUsernameHelpFragment.java
@@ -5,19 +5,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import northseattlecollege.ASLBuddy.R;
 
 /**
  * Created by Sai Chang on 12/9/2016.
  */
 public class SkypeUsernameHelpFragment extends Fragment {
+
+    Fragment help;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         /**
          * Inflate the layout for this fragment
          */
-        return inflater.inflate(R.layout.fragment_skype_profile_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_skype_profile_help, container, false);
 
+        help = this;
 
+        Button back = (Button) view.findViewById(R.id.back);
+        assert back != null;
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getFragmentManager().beginTransaction().remove(help).commit();
+            }
+        });
+
+        return view;
     }
 }
